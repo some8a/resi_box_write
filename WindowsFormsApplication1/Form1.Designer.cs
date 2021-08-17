29bca019d32184c504a3c98408e245056fe2b152
some8a <some8a@gmail.com>
2021-08-17 14:42:29 +0900

    test

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelTeikou = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.buttonUpload = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // labelTeikou
+            // 
+            this.labelTeikou.AutoSize = true;
+            this.labelTeikou.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelTeikou.Location = new System.Drawing.Point(20, 120);
+            this.labelTeikou.Name = "labelTeikou";
+            this.labelTeikou.Size = new System.Drawing.Size(29, 400);
+            this.labelTeikou.TabIndex = 6;
+            this.labelTeikou.Text = " 1\r\n 2\r\n 3\r\n 4\r\n 5\r\n 6\r\n 7\r\n 8\r\n 9\r\n10\r\n11\r\n12\r\n13\r\n14\r\n15\r\n16\r\n17\r\n18\r\n19\r\n20";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.PortName = "COM6";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonStart.Location = new System.Drawing.Point(600, 39);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(206, 32);
+            this.buttonStart.TabIndex = 9;
+            this.buttonStart.Text = "CALCULATE";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonCalc_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(135, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 23);
+            this.textBox1.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(54, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "FILE 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonFile_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.richTextBox2.Location = new System.Drawing.Point(327, 115);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(267, 411);
+            this.richTextBox2.TabIndex = 13;
+            this.richTextBox2.Text = "";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox2.Location = new System.Drawing.Point(408, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(186, 23);
+            this.textBox2.TabIndex = 14;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(327, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 30);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "FILE 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.buttonFile_Click);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.richTextBox3.Location = new System.Drawing.Point(600, 115);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.Size = new System.Drawing.Size(206, 411);
+            this.richTextBox3.TabIndex = 17;
+            this.richTextBox3.Text = "";
+            // 
+            // buttonUpload
+            // 
+            this.buttonUpload.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.buttonUpload.Location = new System.Drawing.Point(600, 77);
+            this.buttonUpload.Name = "buttonUpload";
+            this.buttonUpload.Size = new System.Drawing.Size(206, 32);
+            this.buttonUpload.TabIndex = 18;
+            this.buttonUpload.Text = "UPLOAD";
+            this.buttonUpload.UseVisualStyleBackColor = true;
+            this.buttonUpload.Click += new System.EventHandler(this.buttonUpload_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.richTextBox1.Location = new System.Drawing.Point(54, 115);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(267, 411);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(327, 50);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(83, 16);
+            this.checkBox1.TabIndex = 21;
+            this.checkBox1.Text = "USE FILE 2";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 538);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.buttonUpload);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.labelTeikou);
+            this.Name = "Form1";
+            this.Text = "UPLOAD CALBRATION DATA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTeikou;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button buttonUpload;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using NationalInstruments.VisaNS;
+
+                        
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void setResi(string seriNum)
+        {
+            try
+            {
+                serialPort1.Open();
+                Thread.Sleep(10);
+                serialPort1.Write(seriNum);
+                serialPort1.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        void serialReset()
+        {
+            string[] ptNames = SerialPort.GetPortNames();
+
+            try
+            {
+                serialPort1.PortName = ptNames[0];
+                serialPort1.Open();
+                Thread.Sleep(3000);
+                serialPort1.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonCalc_Click(object sender, EventArgs e)
+        { 
+            string tb1, tb2;
+            double tmpr1, tmpr2, data1, data2 ;
+
+            double[] tb3a = new double[20];
+            double[] tb3b = new double[20];
+
+            richTextBox3.Text = "";
+
+            for (int i = 0; i < tb3a.Length; i++)
+            {
+                tb1 = richTextBox1.Text.Split(',')[i];
+                data1 = double.Parse(tb1.Split('@')[0]);
+                tmpr1 = double.Parse(tb1.Split('@')[1]);
+
+                if (checkBox1.Checked == true)
+                {
+                    tb2 = richTextBox2.Text.Split(',')[i];
+                    data2 = double.Parse(tb2.Split('@')[0]);
+                    tmpr2 = double.Parse(tb2.Split('@')[1]);
+
+                    tb3a[i] = (data1 - data2) / (tmpr1 - tmpr2);
+                    tb3b[i] = data1 - tb3a[i] * tmpr1;
+                }
+                else
+                {
+                    tb3a[i] = 0;
+                    tb3b[i] = data1;
+                }
+
+                if (tb3a[i] < 0)
+                {
+                    if (tb3b[i] < 0)
+                        richTextBox3.Text += "-" + (-tb3a[i] * 1000).ToString("000000000") + "-" + (-tb3b[i]).ToString("000000000") + "\n" ;
+                    else
+                        richTextBox3.Text += "-" + (-tb3a[i] * 1000).ToString("000000000") + "+" + tb3b[i].ToString("000000000") + "\n";
+                }
+                else
+                {
+                    if (tb3b[i] < 0)
+                        richTextBox3.Text += "+" + (tb3a[i] * 1000).ToString("000000000") + "-" + (-tb3b[i]).ToString("000000000") + "\n";
+                    else
+                        richTextBox3.Text += "+" + (tb3a[i] * 1000).ToString("000000000") + "+" + tb3b[i].ToString("000000000") + "\n";
+                }
+            }
+
+        }
+
+        private void buttonFile_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            string savedStr;
+
+            ofd.InitialDirectory = @"C:\Users\denshi\Documents\caldata";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+
+                savedStr = File.ReadAllText(ofd.FileName);
+
+                if (sender == button1)
+                {
+                    textBox1.Text = Path.GetFileName(ofd.FileName);
+                    richTextBox1.Text = savedStr;
+                }
+                if (sender == button2)
+                {
+                    textBox2.Text = Path.GetFileName(ofd.FileName);
+                    richTextBox2.Text = savedStr;
+                }
+
+            }
+        }
+
+        private void buttonUpload_Click(object sender, EventArgs e)
+        {
+            serialReset();
+
+            string UpStr = "C" + richTextBox3.Text.Replace("\n", "");
+
+            setResi(UpStr);
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked == true)
+            {
+                button2.Enabled = true;
+                textBox2.Enabled = true;
+                richTextBox2.Enabled = true;
+            }
+            else
+            {
+                button2.Enabled = false;
+                textBox2.Enabled = false;
+                richTextBox2.Enabled = false;
+            }
+        }
+
+    }
+}
